dfeedfc605a589fb4329bec67fb53bf5fe8f9481
lphnam <lphnam207@gmail.com>
2017-11-21 01:46:52 +0700

    Add GET, GET id, POST method

--- index.php
+++ index.php
@@ -6,36 +6,11 @@ use Medoo\Medoo;
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-
-// include 'db.php';
-// Initialize
-// $database = new Medoo([
-//     'database_type' => 'sqlite',
-//     'database_file' => '/home/lphnam/app/test.db'
-// ]);
-//
-// // Enjoy
-// $database->insert('account', [
-//     'user_name' => 'foo',
-//     'email' => 'foo@bar.com'
-// ]);
-//
-// $account_id = $database->id();
-//
-// $data = $database->select('account', '*');
-//
-// echo json_encode($data);
-// echo $account_id;
-// var_dump($database);
-// $data = $database->select('account', '*');
-// echo json_encode($data);
-
-
 $app = new \Slim\App;
 $app->get('/hello', function (Request $request, Response $response) {
   $database = new Medoo([
     'database_type' => 'sqlite',
-    'database_file' => '/home/lphnam/app/test.db'
+    'database_file' => '/home/lphnam/app/play-with-Medoo/account.db'
   ]);
   $data = $database->select('account', '*');
   $data = json_encode($data);
@@ -48,21 +23,37 @@ $app->get('/hello', function (Request $request, Response $response) {
 $app->get('/hello/{id}', function (Request $request, Response $response) {
     $database = new Medoo([
       'database_type' => 'sqlite',
-      'database_file' => '/home/lphnam/app/test.db'
+      'database_file' => '/home/lphnam/app/play-with-Medoo/account.db'
     ]);
     $id = $request->getAttribute('id');
     $data = $database->select('account', '*', ['id'=>$id]);
     $data = json_encode($data);
 
+    $database->insert("account", [
+    	"user_name" => "foo",
+    	"email" => "foo@bar.com",
+    ]);
+
     $response->getBody()->write($data);
 
     return $response;
 });
+
+$app->post('/hello', function (Request $request, Response $response) {
+    $data = $request->getParsedBody();
+
+    $database = new Medoo([
+      'database_type' => 'sqlite',
+      'database_file' => '/home/lphnam/app/play-with-Medoo/account.db'
+    ]);
+
+    $database->insert("account", [
+    	"user_name" => $data["user_name"],
+    	"email" => $data["email"],
+    ]);
+
+    // $response->getBody()->write($data);
+    return $response;
+});
+
 $app->run();
-
-
-/*
-  GET: URL/hello
-  GET: URL/hello/{id}
-  POST URL/hello
-*/
